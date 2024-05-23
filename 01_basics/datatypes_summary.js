52f31fe8c772d154a9f26d11ff0bb3103f59a470
Sara Fatma <52020808+fatma-1234@users.noreply.github.com>
2024-05-23 11:56:54 +0000

    datatypes_summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -2,4 +2,56 @@
 // Primitive 
 // Non Primitive data type 
 
-// Primitives => 
+// Primitive  => 7 Types (call by value)
+// 7 types => 
+// 1) String,
+// 2) Number,
+// 3) Boolean ,
+// 4) null,
+// 5) undefined ,
+// 6) Symbol(value ko unique bnane ke liye symbol ka use krte hain.) ,
+// 7) BigInt 
+
+// const score = 100 
+// const score = false 
+
+
+// java script is dynamically typed 
+
+// const score = 100
+// const scorevalue =  100.3
+
+
+const IsloggedIn= false 
+const outsideTemp= null
+let userEmail; 
+
+// Reference (Non primitive)
+// Array , Objects , Function
+const heros= ["Mohammad", "Papa", "me"];
+let myObj = {
+    name:"sara",
+    age: 22,
+}
+
+// function(){} syntax of function 
+
+const myFunction = function(){
+    console.log("Hello world")
+
+}
+
+console.log(typeof bigNumber);
+
+ 
+
+
+
+
+
+
+// Reference Type (Non premitive) 
+// 1) Array 
+// 2) Objects 
+// 3) Functions 
+//  
