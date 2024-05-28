71bc1a72a19355ad75436a193a6a994bef1f4fa5
Sara Fatma <52020808+fatma-1234@users.noreply.github.com>
2024-05-28 12:34:33 +0000

    numbers

---
+++ 01_basics/03_numbers.js
@@ -0,0 +1,19 @@
+// Numbers 
+
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+
+// a method for using toString() and when we checked the type of its make string 
+console.log(balance.toString().length);
+
+// 
+console.log(balance.toFixed(2).length); 
+
+
+
+
+
