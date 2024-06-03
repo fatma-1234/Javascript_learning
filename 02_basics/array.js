c722042850d5812690bf86788dd624f260bd6d91
Sara Fatma <52020808+fatma-1234@users.noreply.github.com>
2024-06-03 11:19:48 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,51 @@
+//  array
+
+const myarr= [0,1,2,3,4,5,6,7];
+// const myHeros = ["Sunday", "monday"]
+
+// const myArr2 = new Array(1,2,3,4,5,6)
+// console.log(myarr[1]);
+
+// Methods 
+// push() for adding lement 
+// myArr2.push(6)
+// myArr2.push(7)
+// myArr2.push(8)
+// pop() in this pop method we dont need to paas argument ..this automatically delete last element 
+// myArr2.pop()
+// myArr2.pop()
+
+ // unshift operator will add a eleent in index 0 ..means all elements will be shifted 
+// myArr2.unshift(9)
+
+// shift() methos remove that element wich has been added at the index 
+
+// myArr2.shift()
+// console.log(myArr2);
+
+// include() method is for questionable that if a element is presend or not...and its data type is boolean , 
+// it will give answer in true or false 
+
+// console.log(myArr2.includes(9));
+// console.log(myArr2.includes(9));
+// console.log(myArr2.indexOf(5));
+// console.log(myArr2.indexOf(19));
+
+// const newArr = myArr2.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+// console.log(myArr2);
+
+// slice , splice 
+
+console.log("A" , myarr);
+const myn1 = myarr.slice(1,3);
+console.log(myn1);
+const myn2 = myarr.splice(1,3);
+console.log(myn2);
+
+
+
+
+
+
