276b7baea4636b90a133fee19224f4204c84c64c
Sara Fatma <52020808+fatma-1234@users.noreply.github.com>
2024-06-05 12:28:46 +0000

    Functions

---
+++ 03_basics/01_Functions.js
@@ -0,0 +1,31 @@
+// Functions 
+// console.log("H");
+// console.log("S");
+// console.log("T");
+// console.log("R");
+// console.log("A");
+
+function sayMyName(){
+    console.log("H");
+    console.log("S");
+    console.log("T");
+    console.log("R");
+    console.log("A");
+
+
+}
+sayMyName()
+
+// sayMyName => Reference 
+//  () => execution 
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2); 
+}
+
+addTwoNumbers(3, "4")
+
+
+
+
+
