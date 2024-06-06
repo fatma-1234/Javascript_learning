e20ba813623a1949127412a67150501637baeaa8
Sara Fatma <52020808+fatma-1234@users.noreply.github.com>
2024-06-06 09:08:55 +0000

    Function

--- 03_basics/01_Functions.js
+++ 03_basics/01_Functions.js
@@ -38,7 +38,67 @@ const result= addTwoNumbers(3, 5)
 
 // More method for taking arguments (which value we are paasing ) aur kitne tarike se value li ja skati hai ..
 
-function loginUserMessage 
+function loginUserMessage(username = "sam"){
+    if(username===undefined){
+        console.log("please enter a username ");
+        return 
+
+    }
+    // above written code are same as this code another method for writing /..
+    // if(!username){
+    //     console.log("please enter a username ");
+    //     return 
+    // }
+    return `${username} just logged in`
+
+}
+// console.log(loginUserMessage("sara"))
+// console.log(loginUserMessage())
+
+
+// ... rest operator  or spread operator 
+// function calculateCartPrice(...num1){
+//     return num1 
+// }
+// console.log(calculateCartPrice(200, 300, 500, 700))
+
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+
+}
+console.log(calculateCartPrice(200, 300, 500, 700))
+
+const user = {
+    username : "sara",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+// handleObject(user)
+
+// here function calling 
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 500, 800]
+function returnSecondValue(getArray){
+    return getArray[0]
+
+}
+console.log(returnSecondValue(myNewArray));
+
+
+
+
+
+
 
 
 
