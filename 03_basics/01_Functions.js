9311ca28bcca4209e39ce9a4a157635cad543a5c
Sara Fatma <52020808+fatma-1234@users.noreply.github.com>
2024-06-06 08:05:08 +0000

    function

--- 03_basics/01_Functions.js
+++ 03_basics/01_Functions.js
@@ -1,4 +1,5 @@
-// Functions 
+// Functions
+
 // console.log("H");
 // console.log("S");
 // console.log("T");
@@ -19,11 +20,26 @@ sayMyName()
 // sayMyName => Reference 
 //  () => execution 
 
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2); 
+// }
 function addTwoNumbers(number1, number2){
-    console.log(number1 + number2); 
+    // let result = number1 + number2
+    // return result 
+    return number1 + number2
+   
 }
 
-addTwoNumbers(3, "4")
+
+// addTwoNumbers(3, "4")
+// addTwoNumbers(3, "null")
+const result= addTwoNumbers(3, 5)
+// console.log("Result: ", result);
+
+// More method for taking arguments (which value we are paasing ) aur kitne tarike se value li ja skati hai ..
+
+function loginUserMessage 
+
 
 
 
