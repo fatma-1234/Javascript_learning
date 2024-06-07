b0e16f4de2314eae3d54fdde25eef4f09c71d3b0
Sara Fatma <52020808+fatma-1234@users.noreply.github.com>
2024-06-07 12:19:18 +0000

    one

---
+++ 05_Control_flow/one.js
@@ -0,0 +1,64 @@
+// control flow or logic flow 
+// IF Statement 
+
+// syntax 
+// if (condition){
+// }
+
+// // if(true){   if condition will true internal code  will execute ...
+// // }
+
+// // const ISuserLoggedIn = true 
+// // if (ISuserLoggedIn){
+
+// // }
+
+// // if ( 3!= 2){
+
+// // }
+// // strict equal (===)
+// if (2 != 3){
+//     console.log("executed");
+// }
+
+// // other program 
+// const temparature = 55
+// if (temparature < 50){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("greater than 50")
+
+// }
+
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+
+// }
+// console.log(`user power: ${power}`);
+
+// // short hand notation 
+
+const balance = 1000
+// if (balance > 500) console.log("test"), console.log("test2");
+// never use this typr of code its always not good type of code 
+
+if (balance > 500){
+    console.log("less than ")
+}else if (balance < 750){
+    console.log("greater than 750")
+}else if ( balanace < 900){
+    console.log("less than 750 ")
+}else {
+    console.log("less than 1200")
+}
+
+
+
+
+
+
