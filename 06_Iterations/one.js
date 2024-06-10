a88c2e819a6f4e8ace7859bcfea2a984c8af2480
Sara Fatma <52020808+fatma-1234@users.noreply.github.com>
2024-06-10 12:51:28 +0000

    iterations

---
+++ 06_Iterations/one.js
@@ -0,0 +1,21 @@
+// for 
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5){
+        console.log("5 is the best number");
+
+    }
+    console.log(element);
+}
+
+// console.log(element);
+
+
+
+
