c7fdcf75c247313f1d6cd39b76cde553c3e92a56
Sara Fatma <52020808+fatma-1234@users.noreply.github.com>
2024-06-11 09:21:14 +0000

    Iterations

--- 06_Iterations/one.js
+++ 06_Iterations/one.js
@@ -5,17 +5,57 @@
     
 // }
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (element == 5){
-        console.log("5 is the best number");
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5){
+//         console.log("5 is the best number");
 
-    }
-    console.log(element);
-}
+//     }
+//     console.log(element);
+// }
 
 // console.log(element);
 
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outer loop value: ${i}`);
+//     for (let j =0; j<=10; j++){
+//         console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     }
+    
+    
+// // }
+
+// let myarray = ["flash", "batman", "superman"]
+// console.log(myarray.length);
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);
+// }
+
+// break and continue 
+
+for (let index = 1; index <= 20; index++) {
+    if( index == 5 ) {
+        console.log(`dedected 5`);
+        break
+        
+    }
+    console.log(`value of 1 is ${index}`);
+    
+    
+}
+
+// continue 
+for (let index = 1; index <= 20; index++) {
+    if( index == 5 ) {
+        console.log(`dedected 5`);
+        continue 
+        
+    }
+    console.log(`value of 1 is ${index}`);
+    
+    
+}
 
 
 
